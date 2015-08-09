9e174b0a91a8221ed1dfae766ea462bb9e65716e
Julian Locke <julian.locke@savantsystems.com>
2015-08-09 15:23:36 -0400

    Imports SDK

--- Polo/AppDelegate.swift
+++ Polo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+
         // Override point for customization after application launch.
         return true
     }
